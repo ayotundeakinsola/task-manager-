4ca8d8215db0bc1d3b107c95bd7adf7e7bf6c991
Ayotunde Akinsola <ayotundeakinsola@gmail.com>
2020-06-03 22:45:43 +0000

    Added templates for task

--- app.py
+++ app.py
@@ -7,13 +7,19 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manage'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-qowfo.mongodb.net/task_manage?retryWrites=true&w=majority'
 
+
 mongo = PyMongo(app)
 
 
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", tasks=mongo.db.task.find())
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html',
+                           categories=mongo.db.category.find())
 
 
 if __name__ == '__main__':
